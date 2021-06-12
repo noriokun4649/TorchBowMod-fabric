30e8115e41a0512cc25cb1156e81eaabfb5e3c93
noriokun4649 <noriokun.official.mail@gmail.com>
2021-06-12 20:27:19 +0900

    fix pull animations.

--- src/main/java/mod/torchbowmod/TorchBowModClient.java
+++ src/main/java/mod/torchbowmod/TorchBowModClient.java
@@ -5,17 +5,29 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 
-import static mod.torchbowmod.TorchBowMod.MODID;
-import static mod.torchbowmod.TorchBowMod.TORCH;
+import static mod.torchbowmod.TorchBowMod.*;
 
 @Environment(EnvType.CLIENT)
 public class TorchBowModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        FabricModelPredicateProviderRegistry.register(TORCH_BOW_ITEM, new Identifier("pull"), (itemStack, world, livingEntity, seed) -> {
+            if (livingEntity == null) {
+                return 0.0F;
+            }
+            return livingEntity.getActiveItem() != itemStack ? 0.0F : (itemStack.getMaxUseTime() - livingEntity.getItemUseTimeLeft()) / 20.0F;
+        });
+        FabricModelPredicateProviderRegistry.register(TORCH_BOW_ITEM, new Identifier("pulling"), (itemStack, world, livingEntity, seed) -> {
+            if (livingEntity == null) {
+                return 0.0F;
+            }
+            return livingEntity.isUsingItem() && livingEntity.getActiveItem() == itemStack ? 1.0F : 0.0F;
+        });
         EntityRendererRegistry.INSTANCE.register(TORCH, TorchEntityRender::new);
         ClientPlayNetworking.registerGlobalReceiver(new Identifier(MODID, "spawntorch"), (client, handler, buf, responseSender) -> {
             int entityId = buf.readInt();
