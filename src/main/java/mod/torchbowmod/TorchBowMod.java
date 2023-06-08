edace929528e82d0c0536cf5e4d9bb6db12f0767
noriokun4649 <noriokun.official.mail@gmail.com>
2023-06-09 01:39:08 +0900

    Fix ItemGroup

--- src/main/java/mod/torchbowmod/TorchBowMod.java
+++ src/main/java/mod/torchbowmod/TorchBowMod.java
@@ -6,8 +6,10 @@ import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registry;
 
@@ -18,6 +20,15 @@ public class TorchBowMod implements ModInitializer {
     public static final Item MULCH_TORCH_ITEM = new Item(new Item.Settings().maxCount(64));
     public static final Item TORCH_ARROW_ITEM = new TorchArrow(new Item.Settings().maxCount(64));
     public static final EntityType<TorchEntity> TORCH;
+    public static final ItemGroup TORCH_BOW_TAB = FabricItemGroup.builder()
+            .displayName(Text.translatable("itemGroup.torchbowmod.torchbowmod_tab"))
+            .icon(() -> new ItemStack(TorchBowMod.TORCH_BOW_ITEM))
+            .entries((enabledFeatures, entries) -> {
+                entries.add(TorchBowMod.TORCH_BOW_ITEM);
+                entries.add(TorchBowMod.MULCH_TORCH_ITEM);
+                entries.add(TorchBowMod.TORCH_ARROW_ITEM);
+            })
+            .build();
 
     static {
         TORCH = Registry.register(Registries.ENTITY_TYPE,
@@ -28,14 +39,7 @@ public class TorchBowMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        FabricItemGroup.builder(new Identifier(MODID, "torchbowmod_tab"))
-                .icon(() -> new ItemStack(TorchBowMod.TORCH_BOW_ITEM))
-                .entries((enabledFeatures, entries) -> {
-                    entries.add(TorchBowMod.TORCH_BOW_ITEM);
-                    entries.add(TorchBowMod.MULCH_TORCH_ITEM);
-                    entries.add(TorchBowMod.TORCH_ARROW_ITEM);
-                })
-                .build();
+        Registry.register(Registries.ITEM_GROUP, new Identifier(MODID, "torchbowmod_tab"), TORCH_BOW_TAB);
         Registry.register(Registries.ITEM, new Identifier(MODID, "torchbow"), TORCH_BOW_ITEM);
         Registry.register(Registries.ITEM, new Identifier(MODID, "multitorch"), MULCH_TORCH_ITEM);
         Registry.register(Registries.ITEM, new Identifier(MODID, "torcharrow"), TORCH_ARROW_ITEM);
