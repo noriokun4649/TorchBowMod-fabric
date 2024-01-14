66a3baaa1074c660119f2a1142eb2d09df9683e0
noriokun4649 <noriokun.official.mail@gmail.com>
2024-01-14 12:59:30 +0900

    Change TorchEntity behavior.

--- src/main/java/mod/torchbowmod/TorchEntity.java
+++ src/main/java/mod/torchbowmod/TorchEntity.java
@@ -1,13 +1,12 @@
 package mod.torchbowmod;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.PlantBlock;
+import net.minecraft.block.*;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.Item;
@@ -20,6 +19,7 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 import static mod.torchbowmod.TorchBowMod.*;
+import static net.minecraft.entity.EntityType.LIGHTNING_BOLT;
 import static net.minecraft.state.property.Properties.HORIZONTAL_FACING;
 import static net.minecraft.util.math.Direction.DOWN;
 import static net.minecraft.util.math.Direction.UP;
@@ -46,6 +46,9 @@ public class TorchEntity extends PersistentProjectileEntity {
     protected void onEntityHit(EntityHitResult entityHitResult) {
         super.onEntityHit(entityHitResult);
         Entity entity = entityHitResult.getEntity();
+        if (entity instanceof CreeperEntity creeper){
+            creeperIgnite(creeper);
+        }
         entity.setOnFireFor(5);
     }
 
@@ -54,12 +57,35 @@ public class TorchEntity extends PersistentProjectileEntity {
         super.onBlockHit(blockHitResult);
         HitResult.Type raytraced$type = blockHitResult.getType();
         if (raytraced$type == HitResult.Type.BLOCK) {
+            var statePos = blockHitResult.getBlockPos();
             BlockState blockstate = this.getWorld().getBlockState(blockHitResult.getBlockPos());
-            setTorch(blockHitResult, blockstate, blockHitResult);
+            if (getWorld().getBlockState(statePos).getBlock() == Blocks.TNT){
+                tntIgnite(blockHitResult);
+            }else {
+                setTorch(blockHitResult, blockstate, blockHitResult);
+            }
         }
     }
 
 
+    private void creeperIgnite(CreeperEntity creeper){
+        if (Math.random() < 0.05) {
+            creeper.ignite();
+            var bolt = new LightningEntity(LIGHTNING_BOLT, getWorld());
+            bolt.setPosition(creeper.getBlockPos().toCenterPos());
+            getWorld().spawnEntity(bolt);
+        } else if (Math.random() < 0.3) {
+            creeper.ignite();
+        }
+    }
+
+    private void tntIgnite(BlockHitResult blockHitResult){
+        var world = getWorld();
+        var blockPos = blockHitResult.getBlockPos();
+        TntBlock.primeTnt(world, blockPos);
+        world.removeBlock(blockPos, false);
+        this.remove(RemovalReason.KILLED);
+    }
     private void setTorch(BlockHitResult bloatwares, BlockState blockstate, HitResult raytracedResultIn) {
         BlockPos blockpos = bloatwares.getBlockPos();
         if (!blockstate.isAir()) {
